ce83a2641798b860d7d1c22ff0d457809ac778f4
Klimek Petr <KlimekP@tescosw.loc>
2018-11-29 13:21:18 +0100

    moje uprava

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace DiplomovaPrace
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("ahoj");
         }
     }
 }
