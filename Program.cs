a6e9837d0d1676492ccc38c55d4ab0fb70eca8b9
Klimek Petr <KlimekP@tescosw.loc>
2018-11-29 13:16:25 +0100

    prvni

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiplomovaPrace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
